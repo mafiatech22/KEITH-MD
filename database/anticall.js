fa361b78974946cca70346b060072ea0dea230cc
𝘼𝙧𝙨𝙡𝙖𝙣 𝘾𝙝𝙖𝙪𝙙𝙖𝙧𝙮 <143324285+MARC701@users.noreply.github.com>
2025-08-05 18:09:10 +0500

    Update anticall.js

--- database/anticall.js
+++ database/anticall.js
@@ -4,12 +4,12 @@ const { database } = require('../settings');
 const AntiCallDB = database.define('anticall', {
     status: {
         type: DataTypes.BOOLEAN,
-        defaultValue: true,
+        defaultValue: false,
         allowNull: false
     },
     message: {
         type: DataTypes.STRING,
-        defaultValue: 'Keith declined your 🤙 call',
+        defaultValue: '𝐌𝐀𝐅𝐈𝐀-𝐌𝐃 declined your 🤙 call',
         allowNull: false
     },
     action: {
@@ -40,7 +40,7 @@ async function getAntiCallSettings() {
         return settings;
     } catch (error) {
         console.error('Error getting anti-call settings:', error);
-        return { status: true, message: 'Keith declined your 🤙 call', action: 'reject' };
+        return { status: false, message: '𝐌𝐀𝐅𝐈𝐀-𝐌𝐃 declined your 🤙 call', action: 'reject' };
     }
 }
 
